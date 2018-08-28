22f98c0e1b2f5b3f13881084afce39fc27194964
knguyen-academy <knguyen.academy@gmail.com>
2018-08-28 16:28:53 -0700

    Ex1 - Add MembershipType Name col, and change Customer Index page to display plan

---
+++ MovieRentalMVC/Migrations/201808282253466_AddMemberShipTypeName.cs
@@ -0,0 +1,19 @@
+namespace MovieRentalMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMemberShipTypeName : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String());
+
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
